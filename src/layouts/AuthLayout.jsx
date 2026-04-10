851f120d89a2ef8f7cd19a7ad6453d1ce2dd54d7
LautaroLeall <lautaroleal4@gmail.com>
2026-04-10 12:08:12 -0300

    component/layouts auth (componente padre) and .css

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,7 +1,27 @@
+import { GraduationCap } from "lucide-react";
+import { Link } from "react-router-dom";
+import '../styles/AuthLayout.css';
+
 function AuthLayout({ children }) {
     return (
-        <main className="min-h-screen flex items-center justify-center bg-slate-100">
-            <section className="w-full max-w-md bg-white rounded-xl shadow-lg p-8">
+        <main className="auth-layout-main">
+
+            {/* Soft grid background */}
+            <div className="auth-bg-grid"></div>
+
+            <section className="auth-card">
+                <div className="auth-logo-wrapper">
+                    <Link to="/" className="auth-logo-link">
+                        <div className="auth-logo-icon">
+                            <GraduationCap className="auth-logo-lucide" size={28} />
+                        </div>
+                        <div className="auth-logo-text">
+                            <h1 className="auth-logo-title">StudyMatch</h1>
+                            <span className="auth-logo-subtitle">UNSTA</span>
+                        </div>
+                    </Link>
+                </div>
+
                 {children}
             </section>
         </main>
