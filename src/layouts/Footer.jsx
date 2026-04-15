10cb76bcb965e75e4824a44887f665c5c7068c36
LautaroLeall <lautaroleal4@gmail.com>
2026-04-15 10:14:26 -0300

    update footer (new styles)

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -3,14 +3,26 @@ import '../styles/Footer.css';
 
 function Footer() {
     return (
-        <footer className="footer-wrapper">
-            <div className="footer-container">
-                <div className="footer-logo-group">
-                    <GraduationCap className="footer-icon" size={20} />
-                    <span className="footer-title">StudyMatch UNSTA</span>
+        <footer className="footer-bottom">
+            <div className="footer-bottom-container">
+                <div className="footer-bottom-left">
+                    <div className="footer-bottom-logo-icon">
+                        < GraduationCap size={16} />
+                    </div>
+                    <span className="footer-bottom-brand">StudyMatch UNSTA</span>
                 </div>
-                <p className="footer-text">© 2026 StudyMatch — UNSTA, Campus Yerba Buena
-                </p>
+
+                <div className="footer-bottom-center">
+                    <p className="footer-bottom-info">
+                        Universidad del Norte Santo Tomás de Aquino — Campus Yerba Buena
+                    </p>
+                </div>
+
+                <div className="footer-bottom-right">
+                    <p className="footer-bottom-copy">
+                        Proyecto Final · 2026
+                    </p>
+                </div>
             </div>
         </footer>
     );
