10c14f322730409bf57d03203296bf3b51b2cf71
LautaroLeall <lautaroleal4@gmail.com>
2026-04-15 10:17:58 -0300

    landing page / (call new component CTA)

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -4,6 +4,7 @@ import HeaderLanding from "../../components/sections/HeaderLanding";
 import HeroSection from "../../components/sections/HeroSection";
 import HowItWorksSection from "../../components/sections/HowItWorksSection";
 import FeaturesSection from "../../components/sections/FeaturesSection";
+import CTASection from "../../components/sections/CTASection";
 
 import Footer from "../../layouts/Footer";
 import "../../styles/LandingPage.css";
@@ -22,6 +23,7 @@ function PageLanding() {
             <HeroSection />
             <FeaturesSection />
             <HowItWorksSection />
+            <CTASection />
             <Footer />
         </div>
     );
