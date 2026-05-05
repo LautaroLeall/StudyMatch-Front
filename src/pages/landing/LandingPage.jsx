7236c9313f420f4a425dfe2d562640cc8f60e1ce
LautaroLeall <lautaroleal4@gmail.com>
2026-05-04 23:59:58 -0300

    update landing page (delete hooks welcome user.name)

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -9,10 +9,6 @@ import Footer from "../../layouts/Footer";
 import "../../styles/LandingPage.css";
 
 function PageLanding() {
-    // Extrae el objeto 'user' del contexto de autenticación global.
-    // Si el usuario está logueado (user !== null), se muestra un mensaje de bienvenida.
-    // Si no está logueado (user === null), el mensaje no se renderiza (&&  short-circuit).
-
     return (
         <div className="landing-page-wrapper">
             <HeaderLanding />
